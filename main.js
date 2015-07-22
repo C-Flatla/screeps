51edec946c8f2429b661b66c8b27e17640922312
Flatla, Cody <Cody_Flatla@intuit.com>
2015-07-22 10:31:21 -0600

    Adding guard and changing builder to upgrader

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
 var spawnLogic = require('spawnLogic');
 var harvester = require('harvester');
 var mule = require('mule');
-var builder = require('builder');
+var upgrader = require('upgrader');
 var defaultCreep = require('defaultCreep');
 
 var harvesters = 2;
 var mules = 2;
-var builders = 20;
+var upgraders = 20;
+var guards = 0;
 
 if(Game.spawns.Spawn1.energy >= 300) {
     if(spawnLogic.getCreepNumber('harvester') < harvesters) {
         spawnLogic.generateCreep('harvester');
     } else if(spawnLogic.getCreepNumber('mule') < mules) {
         spawnLogic.generateCreep('mule');
-    } else if(spawnLogic.getCreepNumber('builder') < builders) {
-        spawnLogic.generateCreep('builder');
+    } else if(spawnLogic.getCreepNumber('upgrader') < upgraders) {
+        spawnLogic.generateCreep('upgrader');
+    } else if(spawnLogic.getCreepNumber('guard') < guards) {
+        spawnLogic.generateCreep('guard');
     }
 }
 
 for(var name in Game.creeps) {
     var creep = Game.creeps[name];
 
-    if(creep.memory.role == 'harvester') {
-        harvester(creep);
-    } else if(creep.memory.role == 'mule') {
-        mule(creep);
-    } else if(creep.memory.role == 'builder') {
-        builder(creep);
-    } else if(creep.memory.role == 'guard') {
-        var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-        if(targets.length) {
-            creep.moveTo(targets[0]);
-            creep.attack(targets[0]);
-        }
-    } else {
-        defaultCreep(creep);
+    switch (creep.memory.role) {
+        case 'harvester':
+            harvester(creep);
+            break;
+        case 'mule':
+            mule(creep);
+            break;
+        case 'upgrader':
+            upgrader(creep);
+            break;
+        case 'guard':
+            guard(creep);
+        default:
+            defaultCreep(creep);
     }
 }
